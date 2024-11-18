3290f21e527f13137c4de0aebf14809f8a117e51
Brunocj <a20212474@pucp.edu.pe>
2024-11-18 11:33:54 -0500

    Limpieza de codigo

--- ImplementacionBeta/backend/controllers/MedicionController.js
+++ ImplementacionBeta/backend/controllers/MedicionController.js
@@ -1,6 +1,8 @@
 const MedicionRepository = require('../repositories/MedicionRepository');
 
+//MANEJAR VISTAS
 class MedicionController {
+  /*ENDPOINT
   async getAllMediciones(req, res) {
     try {
       const mediciones = await MedicionRepository.findAllMedicionesWithData();
@@ -8,18 +10,8 @@ class MedicionController {
     } catch (err) {
       res.status(500).json({ message: 'Error al obtener las mediciones', error: err.message });
     }
-  }
+  }*/
 
-  // Nueva función para mostrar la vista con Highcharts
-  async mostrarMedicionesEnVista(req, res) {
-    try {
-      const mediciones = await MedicionRepository.getDecimatedData();
-      res.render('index', { mediciones });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Error al mostrar las mediciones en la vista', error: err.message });
-    }
-  }
 
   // Nueva función para mostrar la vista con Highcharts
   async mostrarMedicionPorIdEnVista(req, res) {
@@ -33,24 +25,16 @@ class MedicionController {
             return res.status(404).send("No se encontraron datos para la medición con ID " + id);
         }
 
-        console.log("Medición enviada al cliente:", JSON.stringify(medicion, null, 2));
+        //Depurar: console.log("Medición enviada al cliente:", JSON.stringify(medicion, null, 2));
 
-        res.render('medicion', { medicion });
+        res.render('VersionBeta/medicion', { medicion });
     } catch (err) {
         console.error("Error al obtener la medición:", err);
         res.status(500).json({ message: `Error al mostrar la medición con ID ${id}`, error: err.message });
     }
 }
 
-  async mostrarMedicionesFull(req, res) {
-    try {
-      const mediciones = await MedicionRepository.findAllMedicionesWithData();
-      res.render('index_fullData', { mediciones });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Error al mostrar las mediciones en la vista', error: err.message });
-    }
-  }
+
 }
 
 module.exports = new MedicionController();
--- ImplementacionBeta/backend/repositories/MedicionRepository.js
+++ ImplementacionBeta/backend/repositories/MedicionRepository.js
@@ -33,7 +33,7 @@ class MedicionRepository {
     }
 
     const decimatedData = [];
-    const decimationFactor = 50;
+    const decimationFactor = 100;
 
     // Agrupar y procesar los datos de sensores
     const groupedBySensor = medicion.data_sensors.reduce((acc, data) => {
@@ -67,49 +67,6 @@ class MedicionRepository {
     return medicion;
 }
 
-  async getDecimatedData() {
-    const repository = this.getRepository();
-    const mediciones = await repository.find({
-      relations: ['data_sensors', 'data_sensors.sensor'],
-    });
-    var decimationFactor = 50;
-    const decimatedData = [];
-
-    mediciones.forEach(medicion => {
-      const dataSensors = medicion.data_sensors;
-
-      // Agrupar por sensor
-      const groupedBySensor = dataSensors.reduce((acc, data) => {
-        const sensorId = data.sensor.idSensor; // ID del sensor
-        if (!acc[sensorId]) {
-          acc[sensorId] = [];
-        }
-        acc[sensorId].push(data);
-        return acc;
-      }, {});
-
-      // Iterar sobre cada grupo de sensores
-      Object.values(groupedBySensor).forEach(sensorGroup => {
-        let index = 1; // Reiniciar índice para cada sensor
-        for (let i = 0; i < sensorGroup.length; i += decimationFactor) {
-          const chunk = sensorGroup.slice(i, i + decimationFactor);
-          const promedio = chunk.reduce((sum, data) => sum + data.valor, 0) / chunk.length;
-
-          decimatedData.push({
-            idData_sensor: chunk[0].idData_sensor, // ID de la primera medición
-            valor: promedio, // Promedio
-            index: index.toString(), // Índice reiniciado para cada sensor
-            idMedicion: medicion.idMedicion, // ID de la medición
-            sensor: chunk[0].sensor, // Sensor asociado
-          });
-
-          index++;
-        }
-      });
-    });
-
-    return decimatedData;
-  }
 }
 
 module.exports = new MedicionRepository();
